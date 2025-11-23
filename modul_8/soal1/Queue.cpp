0646fa10fb70ab867f3928f520ecdc61d28b4608
abdad mubaroq <abdadmubaroq@192.168.92.12>
2025-11-23 16:21:13 +0700

    soal 1 fiks

--- modul_8/soal1/Queue.cpp
+++ modul_8/soal1/Queue.cpp
@@ -15,32 +15,56 @@ bool isFullQueue(Queue Q){
     return (Q.tail == 4);
 }
 void enqueue(Queue &Q, Infotype x){
+    if(isFullQueue(Q)){
+        cout << "queue penuh" << endl;
+
+    }
     if (isEmptyQueue(Q)){
-        Q.head = 0;
-        Q.tail = 0;
-        Q.info[Q.tail] = x;
-    } else {
+        Q.head = 1;
+        Q.tail = 1;
+        Q.info[0] = x;
+    }else{
         Q.tail++;
-        Q.info[Q.tail] = x;
+        Q.info[Q.tail - 1] = x;
     }
+    
 }
+
 Infotype dequeue(Queue &Q){
-    Infotype x = Q.info[Q.head];
-    if (Q.head == Q.tail){
-        Q.head = -1;
-        Q.tail = -1;
-    } else {
-        Q.head++;
-    }
-    return x;
-}
-void printInfoQueue(Queue Q){
     if (isEmptyQueue(Q)){
-        cout << "Queue kosong" << endl;
+        cout << "queue kosong" << endl;
+        return -1;
+    }
+    Infotype hasil = Q.info[0];
+    for (int i = 0; i < Q.tail - 1; i++){
+        Q.info[i] = Q.info[i + 1];
+    }
+    Q.tail--;
+    if (Q.tail == 0){
+        Q.head = 0;
+    }
+    return hasil;
+}
+void printInfoQueue(Queue Q) {
+    int H, T;
+
+    if (isEmptyQueue(Q)) {
+        H = -1;
+        T = -1;
     } else {
-        for (int i = Q.head; i <= Q.tail; i++){
+        H = Q.head - 1;   
+        T = Q.tail - 1;   
+    }
+
+    cout << H << "  -  " << T << "  |  ";
+
+    if (isEmptyQueue(Q)) {
+        cout << "empty queue";
+    } else {
+        for (int i = 0; i < Q.tail; i++) {
             cout << Q.info[i] << " ";
         }
-        cout << endl;
     }
+
+    cout << endl;
 }
