af95d661a9d02a1443ea30d54c01497e3884b341
abdad mubaroq <abdadmubaroq@192.168.92.13>
2025-12-08 07:53:43 +0700

    modul 10 soal 1

---
+++ modul_10/soal1/bstree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "bstree.h"
+using namespace std;
+
+// Alokasi node baru
+address alokasi(infotype x) {
+    address p = new Node;
+    p->info = x;
+    p->left = Nil;
+    p->right = Nil;
+    return p;
+}
+
+// Insert ke BST
+void insertNode(address &root, infotype x) {
+    if (root == Nil) {
+        root = alokasi(x);
+    } else if (x < root->info) {
+        insertNode(root->left, x);
+    } else if (x > root->info) {
+        insertNode(root->right, x);
+    }
+}
+
+// Mencari node
+address findNode(infotype x, address root) {
+    if (root == Nil) return Nil;
+    if (x == root->info) return root;
+    else if (x < root->info) return findNode(x, root->left);
+    else return findNode(x, root->right);
+}
+
+// Traversal InOrder
+void printInOrder(address root) {
+    if (root != Nil) {
+        printInOrder(root->left);
+        cout << root->info << " - ";
+        printInOrder(root->right);
+    }
+}
---
+++ modul_10/soal1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "bstree.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    address root = Nil;
+    insertNode(root,1);
+    insertNode(root,2);
+    insertNode(root,6);
+    insertNode(root,4);
+    insertNode(root,5);
+    insertNode(root,3);
+    insertNode(root,6);   // duplikat → diabaikan
+    insertNode(root,7);
+
+    printInOrder(root);
+    return 0;
+}
